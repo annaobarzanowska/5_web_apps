77e0b509c164ca453ea7fcea456b842dd2724534
AnnaO <anna.obarzanowska@gmail.com>
2022-10-27 19:12:38 +0100

    added projects to debug

---
+++ 4_projects_to_debug/unsecure_app/app.rb
@@ -0,0 +1,21 @@
+require 'sinatra/base'
+require "sinatra/reloader"
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    return erb(:index)
+  end
+
+  post '/hello' do
+    @name = params[:name]
+    if @name.include? '<script'
+      return 'Sorry, incorrect submission.'
+    else
+    return erb(:hello)
+    end
+  end
+end
